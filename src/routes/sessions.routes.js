a016472c371a908c559c4dd200cd1740af22da54
Leonardo Albano Mondini <102124207+LeonardoAlbano@users.noreply.github.com>
2023-12-11 14:10:59 -0300

    Update sessions.routes.js

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,9 +1,17 @@
-const { Router } = require("express")
+// Importa o objeto 'Router' do módulo 'express', que é usado para definir rotas
+const { Router } = require("express");
 
-const SessionsController = require("../controllers/SessionsController")
+// Importa a classe 'SessionsController' que você definiu anteriormente
+const SessionsController = require("../controllers/SessionsController");
+
+// Cria uma instância do 'SessionsController'
 const sessionsController = new SessionsController();
 
+// Cria um objeto 'Router' para gerenciar as rotas relacionadas às sessões
 const sessionsRoutes = Router();
+
+// Define uma rota que responde a requisições POST para o caminho "/" (raiz) usando o método 'create' do 'sessionsController'
 sessionsRoutes.post("/", sessionsController.create);
 
+// Exporta o objeto 'sessionsRoutes' para que ele possa ser usado em outros lugares da aplicação
 module.exports = sessionsRoutes;
